87200718c1804a849e555f78ecd8d3abc54fd076
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-11-20 02:05:36 +0500

    compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
 function App() {
+  const [darkmode, setDarkmode] = useState(false);
+
+  function handleClick() {
+    setDarkmode((prev) => !prev);
+  }
   return (
-    <>
-      <Header />
-      <MainContent />
-    </>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: darkmode ? "#282d35" : "#fff",
+      }}
+    >
+      <Header darkmode={darkmode} toggleDarkMode={handleClick} />
+      <MainContent darkmode={darkmode} />
+    </div>
   );
 }
 
